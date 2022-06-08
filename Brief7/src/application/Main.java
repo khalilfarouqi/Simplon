a8b25e6ee0e6d3fe26cf7acef69dbf61a4a0c7cf
khalilfarouqi <bio.khalil.f@gmail.com>
2022-06-08 17:46:00 +0100

    secounf

--- Brief7/src/application/Main.java
+++ Brief7/src/application/Main.java
@@ -1,11 +1,17 @@
 package application;
 	
+import java.text.MessageFormat;
+
+import javax.annotation.processing.Messager;
 import javax.print.attribute.standard.*;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 
 
@@ -17,33 +23,27 @@ public class Main extends Application {
 //			Scene scene = new Scene(root,400,400);
 //			scene.getStylesheets().load(getClass().getResource("Form.fxml"));
 			
-			TableView<UserAccount> table = new TableView<UserAccount>();
+			/*TableView<UserAccount> table = new TableView<UserAccount>();
 
 		      // Create column UserName (Data type of String).
-		      TableColumn<UserAccount, String> userNameCol //
-		              = new TableColumn<UserAccount, String>("User Name");
+		      TableColumn<UserAccount, String> userNameCol = new TableColumn<UserAccount, String>("User Name");
 
 		      // Create column Email (Data type of String).
-		      TableColumn<UserAccount, String> emailCol//
-		              = new TableColumn<UserAccount, String>("Email");
+		      TableColumn<UserAccount, String> emailCol = new TableColumn<UserAccount, String>("Email");
 
 		      // Create column FullName (Data type of String).
-		      TableColumn<UserAccount, String> fullNameCol//
-		              = new TableColumn<UserAccount, String>("Full Name");
+		      TableColumn<UserAccount, String> fullNameCol = new TableColumn<UserAccount, String>("Full Name");
 
 		      // Create 2 sub column for FullName.
-		      TableColumn<UserAccount, String> firstNameCol //
-		              = new TableColumn<UserAccount, String>("First Name");
+		      TableColumn<UserAccount, String> firstNameCol = new TableColumn<UserAccount, String>("First Name");
 
-		      TableColumn<UserAccount, String> lastNameCol //
-		              = new TableColumn<UserAccount, String>("Last Name");
-
+		      TableColumn<UserAccount, String> lastNameCol = new TableColumn<UserAccount, String>("Last Name");
+		      
 		      // Add sub columns to the FullName
 		      fullNameCol.getColumns().addAll(firstNameCol, lastNameCol);
 
 		      // Active Column
-		      TableColumn<UserAccount, Boolean> activeCol//
-		              = new TableColumn<UserAccount, Boolean>("Active");
+		      TableColumn<UserAccount, Boolean> activeCol = new TableColumn<UserAccount, Boolean>("Active");
 
 		      table.getColumns().addAll(userNameCol, emailCol, fullNameCol, activeCol);
 
@@ -51,7 +51,7 @@ public class Main extends Application {
 		      root.setPadding(new Insets(5));
 		      root.getChildren().add(table);
 
-		      stage.setTitle("TableView (o7planning.org)");
+		      stage.setTitle("TableView (o7planning.org)");*/
 
 			
 			
@@ -60,6 +60,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			
 			primaryStage.setTitle("Gestion de Produit");
+			
 			
 			primaryStage.setScene(scene);
 			primaryStage.show();
