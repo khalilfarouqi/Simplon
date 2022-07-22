51deb135d98e26ee373c4443656f4ca46e858223
khalilfarouqi <bio.khalil.f@gmail.com>
2022-07-22 12:13:18 +0100

    After XML

--- Brief_10/src/main/java/main.java
+++ Brief_10/src/main/java/main.java
@@ -1,9 +1,21 @@
+import implimentation_DAO.*;
+import models.*;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Impliment_Employe metier = new Impliment_Employe();
+		
+		Employe employe = new Employe();
+		employe.setID(1);
+		
+		metier.lister_Tous(employe);
+		
+//		List<Livre> livres = metier.chercherLivresParMC("");
+		System.out.println(employe.toString());
+		
 	}
 
 }
