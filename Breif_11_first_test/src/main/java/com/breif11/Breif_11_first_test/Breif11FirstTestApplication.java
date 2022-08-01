8c178b3fdcef78ddd09dd6209aeb3bd230e4f3cb
khalilfarouqi <bio.khalil.f@gmail.com>
2022-08-01 18:22:05 +0100

    First test for brief 11

---
+++ Breif_11_first_test/src/main/java/com/breif11/Breif_11_first_test/Breif11FirstTestApplication.java
@@ -0,0 +1,13 @@
+package com.breif11.Breif_11_first_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Breif11FirstTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Breif11FirstTestApplication.class, args);
+	}
+
+}
---
+++ Breif_11_first_test/src/test/java/com/breif11/Breif_11_first_test/Breif11FirstTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breif11.Breif_11_first_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Breif11FirstTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
