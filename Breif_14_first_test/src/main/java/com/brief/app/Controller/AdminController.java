9a7c66a030a39df1cb4333c79605c6fe50629ced
khalilfarouqi <bio.khalil.f@gmail.com>
2022-08-22 18:32:22 +0100

    Breif 14 lundi last

--- Breif_14_first_test/src/main/java/com/brief/app/Controller/AdminController.java
+++ Breif_14_first_test/src/main/java/com/brief/app/Controller/AdminController.java
@@ -4,12 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 public class AdminController {
 	
 	@GetMapping({"/list", "/"})
 	public ModelAndView getAllEmployees() {
 		return null;
+	}
+	
+	@GetMapping("/addEmployeeForm")
+	public ModelAndView addEmployeeForm() {
+		ModelAndView mav = new ModelAndView("Add_Employee_Form");
+		Employee newEmployee = new Employee();
+		mav.addObject("employee", newEmployee);
+		return mav;
 	}
 	
 }
